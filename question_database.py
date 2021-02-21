6887743d32c24eff996dab8c5afc3154639e11a4
Gary Lam <garykailam@gmail.com>
2021-02-20 23:38:53 -0800

    Added some documentation, get_total_question_count and get_question_count need to be made static at a later point in time.

--- question_database.py
+++ question_database.py
@@ -17,21 +17,28 @@ class SQLDatabase:
         self.total_question_count = self.get_total_question_count()
 
     def set_category(self, category):
+        """Sets the category to the passed in value, finds the corresponding category id sets category id to that value,
+        finds the question total for the current difficulty and category."""
         self.category = category
         self.category_id = self.category_list[self.category]
         self.total_question_count = self.get_total_question_count()
 
     def set_difficulty(self, difficulty):
+        """Sets the difficulty to the passed in value, finds the question total for current difficulty and category."""
         self.difficulty = difficulty
         self.total_question_count = self.get_total_question_count()
 
     def reset_token(self):
+        """The token is used to keep track of questions retrieved from the api to ensure no duplicate questions are
+        pulled from the api, the token can be reset to reset if all possible questions have been pulled."""
         token_reset = "https://opentdb.com/api_token.php?command=reset&token=" + self.trivia_token
         trivia_token = requests.get(token_reset).json()
         self.trivia_token = trivia_token["token"]
 
     @staticmethod
     def get_category_list():
+        """Queries the api for the categories and corresponding category id's. Converts that to a dictionary and returns
+        it."""
         c_response = requests.get("https://opentdb.com/api_category.php")
         categories = c_response.json()
         category_list = {}
@@ -40,6 +47,9 @@ class SQLDatabase:
         return category_list
 
     def get_total_question_count(self):
+        """Determines the total number of questions for a given category. If no category is specified, returns 200 for
+        each difficulty. With a specified category, gets total number of questions of equal or lesser difficulty and
+        returns it as a dictionary."""
         if self.category:
             difficulty = self.difficulty
             total_question_count = {}
@@ -57,6 +67,8 @@ class SQLDatabase:
         return total_question_count
 
     def get_question_count(self, difficulty):
+        """Queries the api for the number of questions in the specified difficulty in the current category, rounds
+        that number to lowest multiple of 10 and returns it."""
         category_lookup = "https://opentdb.com/api_count.php?category=" + self.category_id
         q_count_response = requests.get(category_lookup)
         q_count = q_count_response.json()
@@ -66,6 +78,8 @@ class SQLDatabase:
         return question_count
 
     def get_questions(self, amount, difficulty):
+        """Queries the api for the the specified number of questions of the category(if one is specified) and
+        difficulty. Formats the response into a list of tuples and returns it."""
         amount_url = "&amount=" + str(amount)
         category_url = ""
         if self.category:
@@ -88,6 +102,10 @@ class SQLDatabase:
         return sql_str
 
     def build_database(self):
+        """Creates the SQLite database and clears out any questions left over from a prior session and creates a new
+        table. Determines the number of questions required (either the value that was passed in or the total number of
+        questions available in the current category and difficulty) and starts pulling question and adding them to the
+        database, starting from the highest possible difficulty and moving to easier ones."""
         try:
             conn = sqlite3.connect(r"trivia_maze_questions.db")
         except Error as e:
@@ -127,6 +145,9 @@ class SQLDatabase:
         conn.commit()
 
     def add_questions(self, difficulty, db_question_count, c):
+        """Determines the highest possible amount of questions that can be obtained from the desired difficulty that
+        maximizes the total number of questions and requests them. Adds these into the database. Returns the total
+        number of questions pulled."""
         amounts = [50, 40, 30, 20, 10]
         remaining = self.total_question_count[difficulty]
         for amount in amounts:
@@ -138,8 +159,8 @@ class SQLDatabase:
         return db_question_count
 
 
-if __name__ == '__main__':
-    db = SQLDatabase()
-    db.set_category("Entertainment: Music")
-    db.set_difficulty("hard")
-    db.build_database()
+# if __name__ == '__main__':
+#     db = SQLDatabase()
+#     db.set_category("Entertainment: Music")
+#     db.set_difficulty("hard")
+#     db.build_database()
